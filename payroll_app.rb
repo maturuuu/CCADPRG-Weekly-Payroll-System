e9d09d699b0d9568a5b988d20c1357c7cdc9f6df
maturuuu <matthew_kristoffer_ong@dlsu.edu.ph>
2024-04-02 14:21:57 +0800

    added setDefaults function

--- payroll_app.rb
+++ payroll_app.rb
@@ -1,3 +1,16 @@
+def setDefaults (dailySalary=500,
+                maxRegHours=8.0,
+                dayType = ['n', 'n', 'n', 'n', 'n', 'r', 'r'],
+                timeIn = Array.new(7, "0900"),
+                timeOut = Array.new(7, "0900"))
+  $dailySalary = dailySalary
+  $maxRegHours = maxRegHours.to_f
+  $hourlySalary = dailySalary/maxRegHours
+  $dayType = dayType.dup
+  $timeIn = timeIn.dup
+  $timeOut = timeOut.dup
+end
+
 def displayMainMenu
   puts "MAIN MENU ----------------"
   puts "[1] : Calculate weekly pay"
@@ -8,40 +21,47 @@ def displayMainMenu
 end
 
 def getTimeOut
-  timeOut = []
+  thisTimeOut = []
   dayCount = 1
 
   puts "\n--------------------------"
-  puts "Please enter time out in military time (HHMM) format\n\n"
+  puts "Please enter time out (HHMM): \n\n"
   7.times do
     print "Time out for day #{dayCount} => "
-    timeOut.push(gets.chomp.to_i)
+    thisTimeOut.push(gets.chomp)
     dayCount+=1
   end
 
-  return timeOut
+  return thisTimeOut
 end
 
 # Defaults and other variables
 
-timeOut = []
+$dailySalary = nil # default
+$maxRegHours = nil # default
+$hourlySalary = nil
+$dayType = nil # default
+$timeIn = nil # default; make timeInInt with .to_i for calculations
+$timeOut = nil # input; make timeOutInt with .to_i for calculations
+
+setDefaults()
 
 # Main program starts here -------------------------------------------
 
 puts "\nWelcome to the weekly payroll calculator powered by Ruby!\n\n"
 loop do
-  displayMainMenu
+  displayMainMenu()
   choice = gets.chomp.to_i
 
   case choice
   when 1
     # Get input
-    timeOut = getTimeOut
-    timeOut.each { |x| puts x}
+    $timeOut = getTimeOut()
+
     # Calculate weekly pay
     # Output breakdown
   when 2
-    # Update default configuration
+    # Update configuration
   when 3
     puts "Exiting program..."
     break
