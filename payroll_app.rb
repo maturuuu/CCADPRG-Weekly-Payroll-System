16731921e15c86f43ece3af6c2fe3a4f4fa5e782
maturuuu <matthew_kristoffer_ong@dlsu.edu.ph>
2024-04-03 17:55:21 +0800

    Converts time in and time out into 2d array of hours per day

--- payroll_app.rb
+++ payroll_app.rb
@@ -1,7 +1,7 @@
 def setDefaults (dailySalary=500,
                 maxRegHours=8.0,
                 dayType = ['n', 'n', 'n', 'n', 'n', 'r', 'r'],
-                timeIn = Array.new(7, "0900"),
+                timeIn = Array.new(7, "0930"),
                 timeOut = Array.new(7, "0900"))
   $dailySalary = dailySalary
   $maxRegHours = maxRegHours.to_f
@@ -35,7 +35,33 @@ def getTimeOut
   return thisTimeOut
 end
 
-# Defaults and other variables
+def calcHours (timeIn, timeOut)
+  hoursArr = Array.new(7){[]}
+  dayIndex = 0
+
+  7.times do # assign each hour of each day to the 2d array hoursArr
+
+    currentTime = timeIn[dayIndex].to_i
+
+    while currentTime != timeOut[dayIndex].to_i do
+      hoursArr[dayIndex] << currentTime
+
+      currentTime += 100
+      if currentTime == 2400
+        currentTime = 0
+      elsif currentTime > 2400
+        currentTime -= 2400
+      end
+    end
+
+  dayIndex+=1
+  end
+
+  #separate hoursArr into categories and add to $dayBreakdown
+
+end
+
+# Defaults and other global variables ---------------------------------------
 
 $dailySalary = nil # default
 $maxRegHours = nil # default
@@ -43,6 +69,11 @@ $hourlySalary = nil
 $dayType = nil # default
 $timeIn = nil # default; make timeInInt with .to_i for calculations
 $timeOut = nil # input; make timeOutInt with .to_i for calculations
+$dayBreakdown = nil # 2d array to store each day's breakdown:
+                    # [reg hours]
+                    # [reg hours (night shift)]
+                    # [overtime hours]
+                    # [overtime hours (night shift)]
 
 setDefaults()
 
@@ -59,6 +90,8 @@ loop do
     $timeOut = getTimeOut()
 
     # Calculate weekly pay
+    calcHours($timeIn, $timeOut)
+
     # Output breakdown
   when 2
     # Update configuration
@@ -69,3 +102,22 @@ loop do
     puts "Invalid choice, please input a number from 1 to 3\n\n"
   end
 end
+
+
+# NOTES --------------------------------------------
+# Per day:
+    # Calculate first 9 hours (8 work hours + 1 unpaid break hour)
+    #   - what type of day?
+    #   - are there hours that go into night shift?
+    # Calculate overtime
+    #   - how many hours of overtime?
+    #   - how many of those hours are night shift?
+    #   - what type of day?
+
+# Calculating the hours:
+    # 1. Add all hours of each day into a 2d array (rows = days)
+    # Per day:
+    #   - add current time to array
+    #   - increment and reset if needed
+    #   - repeat until time out is reached
+    # 2. Separate the hours into categories and add to $dayBreakdown
